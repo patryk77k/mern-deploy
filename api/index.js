237736c2ad646e9168e3100ff9f115096921b0e5
patryk77k <patryk77.k@gmail.com>
2023-04-30 14:10:57 +0200

    initial commit

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+dotenv.config();
+
+app.use(cors());
+
+app.get("/api/test", (req, res) => {
+  res.json("Hello World" + Date.now());
+});
+
+if (process.env.API_PORT) {
+  app.listen(process.env.API_PORT);
+}
+
+module.exports = app;
